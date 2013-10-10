f447ad7a7fdd09447abebf1ecd61f461e28c308e
Simon Ecobar Benitez <s.escob.b.27@gmail.com>
2013-10-09 21:17:33 -0500

    Matrix class for numerical analysis, it has simple gauss formula, partial pivote and total pivote, inverse matrix, matrix multiplication, matrix sum

--- Matrix.rb
+++ Matrix.rb
@@ -28,14 +28,17 @@ class Matrix
             #              @matriz[i][j] = dato
             #       end
             # end
-            @matriz = [ [14,6,-2,3,12], [3,15,2,-5,32], [-7,4,-23,2,-24], [1,-3,-2,16,14] ]
+            # @matriz = [ [14,6,-2,3,12], [3,15,2,-5,32], [-7,4,-23,2,-24], [1,-3,-2,16,14] ]
+            # @matriz = [ [-7,2,-3,4,-12], [5,-1,14,-1,13], [1,9,-7,5,31], [-12,13,-8,-4,-32] ]
+            @matriz = [ [-7,2,-3,4,-12], [5,-1,14,-1,13], [1,9,-7,13,31], [-12,13,-8,-4,-32] ]
       end
 
       def self.imprimir matrix
             puts "Matriz Original:"
             0.upto(matrix.n - 1) do |i|
                   0.upto(matrix.m - 1) do |j|
-                        print "#{matrix.matriz[i][j]} "
+                        # print "#{matrix.matriz[i][j]} "
+                        printf "[%4f]",matrix.matriz[i][j]
                   end
                   puts ""
             end
@@ -141,12 +144,94 @@ class Matrix
             @t_superior
       end
 
+      def mayor_valor_fila matrix, fila, columna
+             mayor = matrix.matriz[fila][columna].abs
+             _fila = fila
+             pos = fila
+             while _fila < matrix.n
+                   value = matrix.matriz[_fila][columna]
+                   if value.abs > mayor then mayor = value ; pos = _fila end
+                   _fila += 1
+             end
+             return mayor, pos
+      end
+
+      def triangular_superior_pivoteo
+            if @t_superior.nil?
+                   @t_superior ||= self.clone
+                   # pivoteo_parcial
+                   pivoteo_total
+            end
+            @t_superior
+      end
+
+      def pivoteo_parcial pos_i = 0, pos_j = 0
+            return if pos_i >= @n - 1 || pos_j >= @m - 1
+            temp = pos_i
+            mayor, fila = mayor_valor_fila @t_superior, temp, pos_j
+            raise "No hay solucion unica" if mayor == 0
+            @t_superior.matriz[pos_i], @t_superior.matriz[fila] = @t_superior.matriz[fila], @t_superior.matriz[pos_i]
+            while temp < @n - 1 do
+                  multiplicador = (@t_superior.matriz[temp + 1][pos_j]).fdiv( mayor )
+                  vector1 = @t_superior.matriz[pos_i].map { |value| value * multiplicador }
+                  @t_superior.matriz[temp + 1].map! { |value| value - vector1.shift }
+                  temp += 1
+            end
+            pivoteo_parcial pos_i + 1, pos_j + 1
+      end
+
+      def seleccionar_mayor matrix, from_fila, from_col
+        mayor = matrix.matriz[from_fila][from_col];
+        pos_fila  = from_fila
+        pos_columna = from_col
+        matriz = matrix.matriz
+        for fila in from_fila ... @n
+          for columna in from_col ... ( @m - 1)
+            value = matriz[fila][columna]
+            if value.abs > mayor 
+              mayor = value
+              pos_fila = fila
+              pos_columna = columna
+            end
+          end
+        end
+        return mayor, pos_fila, pos_columna
+      end
+
+      def rotar_columnas matrix, mayor, from_fila, from_col, col_mayor
+        for _fila in from_fila ... @n
+          matrix.matriz[_fila][from_col], matrix.matriz[_fila][col_mayor] =
+                          matrix.matriz[_fila][col_mayor], matrix.matriz[_fila][from_col]
+        end
+      end
+
+      def pivoteo_total pos_i = 0, pos_j = 0
+        x_vector ||= (1 .. @n).to_a
+        return if pos_i >= @n - 1 || pos_j >= @m - 1
+        mayor, fila_mayor, col_mayor = seleccionar_mayor @t_superior, pos_i, pos_j 
+        @t_superior.matriz[pos_i], @t_superior.matriz[fila_mayor] =
+                          @t_superior.matriz[fila_mayor], @t_superior.matriz[pos_i]
+        rotar_columnas @t_superior, mayor, pos_i, pos_j, col_mayor
+        temp = pos_i
+        raise "No hay solucion unica" if mayor == 0
+        while temp < @n - 1 do
+              multiplicador = (@t_superior.matriz[temp + 1][pos_j]).fdiv( mayor )
+              vector1 = @t_superior.matriz[pos_i].map { |value| value * multiplicador }
+              @t_superior.matriz[temp + 1].map! { |value| value - vector1.shift }
+              temp += 1
+        end
+        Matrix.imprimir @t_superior
+        pivoteo_total pos_i + 1, pos_j + 1
+      end
+
       def triangular_inferior
         
       end
 end
 
 test = Matrix.new 4, 5
-Matrix.imprimir test
-Matrix.imprimir test.transpuesta
-Matrix.imprimir test.triangular_superior
+# Matrix.imprimir test
+# Matrix.imprimir test.transpuesta
+# Matrix.imprimir test.triangular_superior
+# Matrix.imprimir test.triangular_superior_pivoteo
+Matrix.imprimir test.triangular_superior_pivoteo
